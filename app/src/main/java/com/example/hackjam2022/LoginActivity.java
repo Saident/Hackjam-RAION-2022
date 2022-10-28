0591427f8b853cb6d742b2c527a5ce61a409a579
Saident <averilprimayuda@gmail.com>
2022-10-28 15:40:08 +0700

    login

--- app/src/main/java/com/example/hackjam2022/LoginActivity.java
+++ app/src/main/java/com/example/hackjam2022/LoginActivity.java
@@ -1,4 +1,44 @@
 package com.example.hackjam2022;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bt_login;
+    public EditText etEmail, etPassword;
+    TextView forgotpass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        bt_login = (Button) findViewById(R.id.bt_login);
+        bt_login.setOnClickListener(this);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bt_login:
+//                Loginuser();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+//            case R.id.register:
+//                startActivity(new Intent(this, RegisterActivity.class));
+//                break;
+            case R.id.forgot:
+                break;
+        }
+    }
 }
